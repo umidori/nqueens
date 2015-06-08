48fd05f644eddfd65c06680dc33585359ad544a1
Toriumi Hidekazu <tori.bj@gmail.com>
2015-06-09 00:27:03 +0900

    Remove callee

--- procedural/queens.js
+++ procedural/queens.js
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
 
 function nqueens(size) {
+    'use strict';
     var row=[], up=[], down=[], board=[];
 
-    (function(n) {
+    (function queen(n) {
         if (n >= size) {
             console.log(board.join(' '));
         } else {
             for (var i = 0; i < size; ++i) {
                 if (!row[i] && !up[n+i] && !down[n-i+size-1]) {
-                    row[i]=up[n+i]=down[n-i+size-1]=1
+                    row[i]=up[n+i]=down[n-i+size-1]=1;
                     board[n] = i+1;
-                    arguments.callee(n+1);
-                    row[i]=up[n+i]=down[n-i+size-1]=0
+                    queen(n+1);
+                    row[i]=up[n+i]=down[n-i+size-1]=0;
                 }
             }
         }
-    })(0);
+    }(0));
 }
 
 nqueens(process.argv[2]);
